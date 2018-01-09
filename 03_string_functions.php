6677a42722d0e021740e7a6a38b9d2da51d717b3
MichielVE-immalle <michiel.vaneester@immalle.be>
2018-01-09 15:08:22 +0100

    oef 4 opgelost

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -3,9 +3,12 @@
 $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
-
+echo "<p>hoe veel woorden zitten er in de zin?</p>";
+echo str_word_count($zin);
+echo "<br>";
 
 // Vervang in $zin "lente" door "zomer" en druk af.
-
+$zin = str_replace('lente', 'zomer', $zin);
+echo $zin;
 
 ?>
