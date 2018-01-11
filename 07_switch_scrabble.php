b5364ec0565cdb2e60f21fcc61fa9a06abcce950
MichielVE-immalle <michiel.vaneester@immalle.be>
2018-01-11 10:02:07 +0100

    Oef 8 opgelost

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -16,4 +16,37 @@ $punten = 0;
 // N 1 punt
 
 
+$value = str_split($woord, 1);
+
+for ($x = 0; $x < strlen($woord); $x++){
+	switch ($value[$x]) {
+	    case "q":
+		$punten += 10;
+		break;
+	    case "u":
+		$punten += 4;
+		break;
+	    case "o":
+		$punten += 1;
+		break;
+	    case "t":
+		$punten += 2;
+		break;
+	    case "e":
+		$punten += 1;
+		break;
+	    case "r":
+		$punten += 2;
+		break;
+	    case "i":
+		$punten += 1;
+		break;
+	    case "n":
+		$punten += 1;
+		break;
+	}
+}
+
+echo "<p>Wanneer men scrabble speelt zou men voor het woord \"$woord\" $punten punten krijgen.</p>";
+
 ?>
